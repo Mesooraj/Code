654d38c3e6e8094f00782d26ff3e853f7b6fb33f
Mesooraj <mesoorajs@gmail.com>
2024-04-24 16:10:46 +0530

    footer settings

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ let counter = 0;
 const totalSlides = slides.length;
 
 slides.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`;
+  slide.style.left = `${index * 100}%`;
 });
 
 const goback = () => {
-    counter--;
-    if (counter < 0) {
-        counter = totalSlides - 1;
-    }
-    slideImage();
+  counter--;
+  if (counter < 0) {
+    counter = totalSlides - 1;
+  }
+  slideImage();
 };
 
 const gonext = () => {
-    counter++;
-    if (counter >= totalSlides) {
-        counter = 0;
-    }
-    slideImage();
+  counter++;
+  if (counter >= totalSlides) {
+    counter = 0;
+  }
+  slideImage();
 };
 
 const slideImage = () => {
-    slides.forEach((slide) => {
-        slide.style.transform = `translateX(-${counter * 100}%)`;
-    });
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 };
 
 let slideInterval = setInterval(gonext, 2000); // Automatic sliding every 2 seconds
 
 // Stop automatic sliding on mouse hover
 main.addEventListener("mouseover", () => {
-    clearInterval(slideInterval);
+  clearInterval(slideInterval);
 });
 
 // Resume automatic sliding on mouse leave
 main.addEventListener("mouseleave", () => {
-    slideInterval = setInterval(gonext, 2000);
+  slideInterval = setInterval(gonext, 2000);
 });
